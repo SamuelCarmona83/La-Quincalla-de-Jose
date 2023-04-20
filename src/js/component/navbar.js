f24980c48db5678b5987197ae6184e2972a3c945
SamuelCarmona83 <samuel.carmona.rodrigz@gmail.com>
2023-04-20 17:31:14 -0400

    :zap: Reset proyect to default

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export class Navbar extends React.Component {
-	render() {
-		return (
+const Navbar = () => {
+		return (<>
 			<nav className="navbar navbar-light bg-light mb-3">
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
@@ -14,6 +13,6 @@ export class Navbar extends React.Component {
 					</Link>
 				</div>
 			</nav>
+			</>
 		);
-	}
-}
+};
--- src/js/layout.js
+++ src/js/layout.js
@@ -1,12 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-// import ScrollToTop from "./component/scrollToTop";
-
 import injectContext from "./store/appContext";
-
-import { Contacts } from "./views/Contacts.js";
-import { AddContact } from "./views/AddContact.js";
+import { Home } from "./views/home";
 
 export const Layout = () => {
 	return (
@@ -14,11 +9,7 @@ export const Layout = () => {
 			<BrowserRouter>
 				<div>
 					<Switch>
-						<Route exact path="/index.html" component={Contacts} />
-						<Route exact path="/" component={Contacts} />
-						<Route exact path="/contacts" component={Contacts} />
-						<Route exact path="/add" component={AddContact} />
-						<Route exact path="/edit" component={AddContact} />
+						<Route exact path="/" component={Home} />
 						<Route render={() => <h1 className="notfound">Not found!</h1>} />
 					</Switch>
 				</div>
