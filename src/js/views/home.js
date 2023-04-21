52a881fe7a45a8f6e15bff920e95f1bd4dbeb1ff
SamuelCarmona83 <samuel.carmona.rodrigz@gmail.com>
2023-04-21 11:29:56 -0400

    :herb: Home Compoent updated to FakeStoreProducts

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,17 +1,39 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 import "../../styles/home.scss";
 
 export const Home = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		fetch("https://fakestoreapi.com/products")
+			.then(res => res.json())
+			.then(json => setProducts(json));
+	}, []);
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button, bootstrap is working
-			</a>
+			<h1>Store fakeAPI</h1>
+			<div style={{ display: "flex", flexWrap: "wrap" }}>
+				{products.map((item, key) => (
+					<div className="col-3" key={key}>
+						<div className="card m-2">
+							<img
+								src={item.image}
+								className="card-img-top"
+								style={{ maxHeight: "30vh", objectFit: "contain", padding: "3vh" }}
+								alt={item.description}
+							/>
+							<div className="card-body">
+								<h5 className="card-title text-truncate">{item.title}</h5>
+								<p className="card-text text-truncate">{item.description}</p>
+								<a href="#" className="btn btn-primary">
+									Add to cart
+								</a>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
