f29d12754c55c124163d2500c75cc0f09d51d835
SamuelCarmona83 <samuel.carmona.rodrigz@gmail.com>
2023-04-27 14:37:57 -0400

    :herb: Navbar & Carrito integrated for sidebar

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -5,7 +5,47 @@ const getState = ({ getStore, setStore }) => {
 			//Your data structures, A.K.A Entities
 			title: "La quincalla de Jose.",
 			products: [],
-			carrito: []
+			carrito: [
+				{
+					id: 17,
+					title: "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+					price: 39.99,
+					description:
+						"Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
+					category: "women's clothing",
+					image: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
+					rating: {
+						rate: 3.8,
+						count: 679
+					}
+				},
+				{
+					id: 16,
+					title: "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
+					price: 29.95,
+					description:
+						"100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
+					category: "women's clothing",
+					image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
+					rating: {
+						rate: 2.9,
+						count: 340
+					}
+				},
+				{
+					id: 3,
+					title: "Mens Cotton Jacket",
+					price: 55.99,
+					description:
+						"great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+					category: "men's clothing",
+					image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+					rating: {
+						rate: 4.7,
+						count: 500
+					}
+				}
+			]
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
--- src/js/views/Navbar.js
+++ src/js/views/Navbar.js
@@ -1,11 +1,11 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext.js";
-
+import "./Navbar.css";
 const Navbar = () => {
 	const { store } = useContext(Context);
 
-	const [isOpen, setIsOpen] = useState(false); // locales
+	const [isOpen, setIsOpen] = useState(true); // locales
 
 	return (
 		<>
@@ -14,30 +14,87 @@ const Navbar = () => {
 					<Link to="/" className="navbar-brand">
 						{store.title}
 					</Link>
-					<form className="d-flex" role="search">
+					<div className="d-flex" role="search">
 						<input className="form-control mx-2 rounded-0" type="search" placeholder="Search" />
 						<div className="btn btn-outline-dark mx-2 rounded-0">Search</div>
 						<Link to="/products/new" className="btn btn-outline-dark mx-2 rounded-0">
 							New
 						</Link>
-						<div className="dropdown">
-							<div onClick={() => setIsOpen(!isOpen)} className="btn btn-dark dropdown-toggle rounded-0">
-								<i className="fa-brands fa-opencart"></i>
-							</div>
-							<div className={`dropdown-menu dropdown-menu-right rounded-0 ${isOpen ? "show" : ""}`}>
-								{store.carrito.map((producto, index) => (
-									<div key={index} className="dropdown-item">
-										<img
-											src={producto.image}
-											alt={producto.title + index}
-											style={{ width: "40px", height: "40px", borderRadius: "3px" }}
-										/>
-										{producto.title}
-									</div>
-								))}
+						<div onClick={() => setIsOpen(!isOpen)} className="btn btn-dark rounded-0">
+							<i className="fa-brands fa-opencart"></i>
+						</div>
+						<div className="" style={{ position: "relative" }}>
+							<div
+								className={(!isOpen ? "sidebarClose" : "sidebarOpen") + " sidebar overflow-auto "}
+								style={{
+									height: "100vh",
+									width: "50%",
+									position: "fixed",
+									top: 0,
+									right: 0,
+									backgroundColor: "#eee",
+									zIndex: 100
+								}}>
+								<div
+									className="p-4"
+									onClick={() => setIsOpen(!isOpen)}
+									style={{
+										position: "absolute",
+										top: 0,
+										right: 0,
+										display: isOpen ? "block" : "none"
+									}}>
+									<i className="fa-solid fa-xmark"></i>
+								</div>
+								<h3 className="my-5 text-center">{store.title}</h3>
+								<div className="" style={{ overflow: "hidden" }}>
+									{store.carrito.map((producto, index) => (
+										<div key={index} className="">
+											<div className="card m-3 rounded-0">
+												<div className="row no-gutters p-2">
+													<div className="col-md-4 d-flex">
+														<img
+															// style={{ maxHeight: "30vh" }}
+															className="card-img my-auto"
+															src={producto.image}
+															alt={producto.title + index}
+														/>
+													</div>
+													<div className="col-md-8">
+														<div className="card-body">
+															<h5 className="card-title">{producto.title}</h5>
+															<h2 className="card-text font-weight-bolder">
+																$ {producto.price}
+															</h2>
+															<p className="card-text">
+																<small className="text-muted">
+																	{producto.description}
+																</small>
+															</p>
+															<div className="btn-group mx-2" role="group">
+																<button type="button" className="btn btn-secondary">
+																	<i className="fa-solid fa-plus"></i>
+																</button>
+																<button
+																	type="button"
+																	className="btn btn-outline-secondary">
+																	1
+																</button>
+																<button type="button" className="btn btn-secondary">
+																	<i className="fa-solid fa-minus"></i>
+																</button>
+															</div>
+															<button className="btn btn-danger">Remover</button>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									))}
+								</div>
 							</div>
 						</div>
-					</form>
+					</div>
 				</div>
 			</nav>
 		</>
